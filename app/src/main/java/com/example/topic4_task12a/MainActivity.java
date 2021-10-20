3364c8f3c3001f2b0f81d603c16a0a5d81c582b5
jjaca <jjaca@cifpfbmoll.eu>
2021-10-20 19:42:34 +0200

    Ahora gira y se guarda el número

--- app/src/main/java/com/example/topic4_task12a/MainActivity.java
+++ app/src/main/java/com/example/topic4_task12a/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.topic4_task12a;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -17,6 +18,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // miro si hay algo guardado
+        if (savedInstanceState != null) {
+            numero = savedInstanceState.getInt("Guardado");
+            Log.i("Ciclo de vida", String.valueOf(numero));
+        }
 
         contador = (TextView)  findViewById(R.id.show_count);
         //forma de marat de pasar el valor de la variable int con el setText
@@ -36,9 +43,16 @@ public class MainActivity extends AppCompatActivity {
 
     public void sumarUno(View view) {
         numero += 1;
-        //forma de pasar un int a un setText
+        //forma de pasar un int a un setText StackOverflow
         contador.setText(String.valueOf(numero));
     }
 
+    //guardo el estado del contador cuanod giro el movil
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i("Ciclo de vida", "guardado"+ numero);
+        savedInstanceState.putInt("Guardado", numero);
+    }
 
 }
